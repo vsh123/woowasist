b952d72ea71674b504d91b2c20eaa664fbde1ba2
SunHo Van <vsh123@naver.com>
2019-06-03 20:40:53 +0900

    init : first init

---
+++ src/test/java/com/woowa/javis/JavisApplicationTests.java
@@ -0,0 +1,16 @@
+package com.woowa.javis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavisApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
